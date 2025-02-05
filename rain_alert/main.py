f788a53e699518272a3e8c8bc3d43fad2eda0924
jpcal <jpcalsavara@gmail.com>
2025-02-05 13:46:07 -0300

    Private API programs

---
+++ rain_alert/main.py
@@ -0,0 +1,41 @@
+import os
+import requests
+from twilio.rest import Client
+from dotenv import load_dotenv
+
+load_dotenv()  # Carrega as variáveis do arquivo .env
+
+key = os.getenv("OPENWEATHER_API_KEY")
+account_sid = os.getenv("TWILIO_ACCOUNT_SID")
+auth_token = os.getenv("TWILIO_AUTH_TOKEN")
+
+url = "https://api.openweathermap.org/data/2.5/forecast"
+parameters = {
+    "lon": -47.524784,
+    "lat": -23.211178,
+    "appid": key,
+    "cnt": 4,
+    "units": "metric",
+    "lang": "pt"
+}
+
+response = requests.get(url, params=parameters)
+if response.status_code == 200:
+    weather_data = response.json()
+else:
+    print(f"Erro na requisição: {response.status_code}")
+
+will_rain = False
+for index in range(0, 4):
+    weather_code = weather_data["list"][index]["weather"][0]["id"]
+    if int(weather_code) < 700:
+        will_rain = True
+
+if will_rain:
+    client = Client(account_sid, auth_token)
+    message = client.messages.create(
+        from_="whatsapp:+14155238886",
+        body="Vai chover hoje. Lembre-se de levar um guarda-chuva!",
+        to="whatsapp:+5515996690551"
+    )
+    print(message.sid)
